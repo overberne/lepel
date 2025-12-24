a94f5302803b3d9718c138939dfa95b878e1c8c2
Onno Verberne <backshifted@gmail.com>
2025-12-24 01:20:18 +0100

    Remove unused code/files

--- src/lepel/config_/__init__.py
+++
@@ -1,8 +0,0 @@
-# pyright: reportUnusedImport=false
-from lepel.config_.decorator import config_setting
-from lepel.config_.validation import (
-    bind_config_values,
-    ensure_required_config_values,
-    get_config,
-    resolve_config_value,
-)
--- src/lepel/steps/ensure_required_config_values.py
+++
@@ -1,7 +0,0 @@
-from lepel.config_ import ensure_required_config_values
-from lepel import PipelineStep
-
-
-class EnsureRequiredConfigValues(PipelineStep):
-    def run(self) -> None:
-        ensure_required_config_values()
